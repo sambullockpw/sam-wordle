0d5108282350a6d8f310ff9c9f172ee373af3858
Sam Bullock <sam.b.bullock@gmail.com>
2022-03-02 14:08:59 +1300

    adjusted websocket url for heroku deployment

--- wordle/main.js
+++ wordle/main.js
@@ -75,7 +75,7 @@ function showMessage(message) {
 
 function getWebSocketServer() {
     if (window.location.host === "sambullockpw.github.io") {
-        return "wss://connect1111.herokuapp.com/";
+        return "wss://sam-wordle.herokuapp.com/";
     } else if (window.location.host === "localhost:8000") {
         return "ws://localhost:8001/";
     } else {
